fb2d865d65e0e025b85f7616ebe36e1483ea85e5
Pedro Ávila <pedroavila95@gmail.com>
2018-02-14 21:02:19 +0000

    Add fetchUsers action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { FETCH_USERS } from './types'
+
+export const fetchUsers = () => ({
+  type: FETCH_USERS,
+  payload: [
+    { name: 'Jane' },
+    { name: 'Alex' },
+    { name: 'Jim' }
+  ]
+})
