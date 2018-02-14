f8d457e38ac3e04e5ef1c169175e2cf2043977b5
Pedro Ávila <pedroavila95@gmail.com>
2018-02-14 20:37:29 +0000

    Setup Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
 
 import App from './components/App'
 
 render(
-  <App />
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
   ,document.getElementById('root')
 )
